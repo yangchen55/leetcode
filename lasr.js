6fe9630449fca8481acbe27d002cd6c14737685d
yangchen55 <tseyang986@gmail.com>
2023-06-20 22:10:13 +1000

    fgj

---
+++ lasr.js
@@ -0,0 +1,18 @@
+
+const ray = [null, {}, 3]
+const test = function (ray) {
+    if (ray.length === 0) {
+        return ray.length - 1
+    } else {
+        const last = ray[ray.length - 1]
+        return last
+
+    }
+
+
+
+}
+
+
+
+test(ray)
--- test.js
+++ test.js
@@ -1,14 +1,24 @@
 /**
- * @param {string} s
- * @return {boolean}
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
  */
-s = "() {} []"
-var isValid = function (s) {
-    if (s !== "()" && s !== "[]" && s !== "{}" && s !== "() {} []") {
-        console.log("false")
+nums1 = [1, 3], nums2 = [2]
+var findMedianSortedArrays = function (nums1, nums2) {
+    const total = [...nums1, ...nums2].sort((a, b) => a - b)
+    const middleIndex = Math.floor(total.length / 2);
+
+    console.log(total)
+    if (total.length % 2 === 0) {
+        const result = ((total[middleIndex - 1]) + (total[middleIndex])) / 2
+        console.log(total, result)
     } else {
-        console.log("true")
+        // const average = total.reduce((accumulater, currentValue) => accumulater + currentValue) / total.length
+        const result = total[middleIndex]
+        console.log(result)
+
     }
 
 };
-isValid(s)
+
+findMedianSortedArrays(nums1, nums2)
